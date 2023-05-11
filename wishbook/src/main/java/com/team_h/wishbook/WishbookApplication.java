1a766518f92d300a0113a819a266100c3b25ced6
Darak <84322513+luanwiz0@users.noreply.github.com>
2023-05-11 18:32:42 +0900

    Add files via upload

---
+++ wishbook/src/main/java/com/team_h/wishbook/WishbookApplication.java
@@ -0,0 +1,13 @@
+package com.team_h.wishbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishbookApplication.class, args);
+	}
+
+}
